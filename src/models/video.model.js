e0d984e2035e3917c9b75146858d8ae73b134c0d
nikhilgautam96 <nikhilgautam1729@gmail.com>
2023-11-15 22:40:17 +0530

    jwt and bcrypt implemented

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema, model } from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const videoSchema = new Schema(
     {
@@ -38,4 +39,5 @@ const videoSchema = new Schema(
     { timestamps: true }
 );
 
+videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = new model('Video', videoSchema);
